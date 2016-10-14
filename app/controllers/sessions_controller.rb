93fa34ecba97e36c83f59eb9285a4306565e93bd
John Chow <JChow417@gmail.com>
2016-10-14 04:15:10 +0000

    fixed login logout bug, login validation messages implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,32 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if @user && @user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
-      @session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      @messages = []
+      if user
+        @messages << "Incorrect Password" if params[:password].length != 0
+      else
+        @messages << "User does not exist" if params[:email].length != 0
+      end
+
+      @messages << "Email is blank" if params[:email].length == 0
+      @messages << "Password is blank" if params[:password].length == 0
+
+      render :new
+      # redirect_to '/login'
     end
   end
 
   def destroy
-    @session[:user_id] = nil
+    session[:user_id] = nil
     redirect_to '/login'
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/', notice: 'Category created!'
+      redirect_to '/'
     else
       render :new
     end
